3768e2178cf9b7a5290f56e14afcc6ce0cf1ac7c
IsaMontoya17 <isabelamontoya2021@gmail.com>
2024-03-07 16:07:49 -0500

    calculadora terminada

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,47 +1,62 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
-let pregunta = prompt("Qué desea hacer?: \n1.Suma\n2.Resta\n3.Multiplicación\n4.división\n");
-pregunta = pregunta.toLowerCase(); 
+console.log("Qué desea hacer?: ");
+console.log("1. Suma");
+console.log("2. Resta");
+console.log("3. Multiplicación");
+console.log("4. División");
+
+let opcion = prompt("Ingrese el número de la operación que desea realizar: ");
+opcion = opcion.toLowerCase(); 
 
 let num1 = prompt("Ingrese el numero 1: ");
 let num2 = prompt("Ingrese el numero 2: ");
 num1 = parseFloat(num1);
 num2 = parseFloat(num2);
 
-if(pregunta === "1" || pregunta === "suma") {
-    let sum = suma(num1,num2);
-    console.log("La suma de "+ num1 + " y " + num2 + " es: " + sum);
-} else if(pregunta === "2" || pregunta === "resta") {
-    let rest = resta(num1,num2);
-    console.log("La resta de "+ num1 + " y " + num2 + " es: " + rest);
-} else if(pregunta === "3" || pregunta === "multiplicacion") {
-    let multi = multiplicacion(num1,num2);
-    console.log("La multiplicacion de "+ num1 + " y " + num2 + " es: " + multi);
-} else if(pregunta === "4" || pregunta === "division") {
-    let div = division(num1,num2);
-    console.log("La division de "+ num1 + " entre " + num2 + " es: " + div);
+let resultado;
+
+switch(opcion) {
+    case "1":
+    case "suma":
+        resultado = suma(num1, num2);
+        console.log("La suma de "+ num1 + " y " + num2 + " es: " + resultado);
+        break;
+    case "2":
+    case "resta":
+        resultado = resta(num1, num2);
+        console.log("La resta de "+ num1 + " y " + num2 + " es: " + resultado);
+        break;
+    case "3":
+    case "multiplicacion":
+        resultado = multiplicacion(num1, num2);
+        console.log("La multiplicación de "+ num1 + " y " + num2 + " es: " + resultado);
+        break;
+    case "4":
+    case "division":
+        resultado = division(num1, num2);
+        console.log("La división de "+ num1 + " entre " + num2 + " es: " + resultado);
+        break;
+    default:
+        console.log("Opción no válida");
 }
 
 function suma(num1, num2){
-    let suma;
-    suma = num1 + num2;
-    return suma;
+    return num1 + num2;
 }
 
 function resta(num1, num2){
-    let resta;
-    resta = num1 - num2;
-    return resta;
+    return num1 - num2;
 }
 
 function multiplicacion(num1, num2){
-    let multiplicacion;
-    multiplicacion = num1 * num2;
-    return multiplicacion;
+    return num1 * num2;
 }
 
 function division(num1, num2){
-    let division;
-    division = num1 / num2;
-    return division;
+    if(num2 === 0) {
+        return "No se puede dividir por cero";
+    } else {
+        return num1 / num2;
+    }
 }
